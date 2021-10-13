aebc985271bed99004382090f43940a78f48de79
bumbutudor <bumbutudor10@gmail.com>
2021-10-13 21:21:05 +0300

    run transliteration script from php

--- src/Api/Controllers/FindTransTexts.php
+++ src/Api/Controllers/FindTransTexts.php
@@ -97,8 +97,8 @@ class FindTransTexts extends AbstractListController {
 
             // odata fisierul OCR gasit pornim procesul de transliterare
             if (file_exists($cyrTextPath)) {
-                $command = escapeshellcmd($transScriptPath);
-                // $output = shell_exec($command);
+                $command = escapeshellcmd('python3 ' . $transScriptPath . ' ' . $cyrTextPath);
+                $output = shell_exec($command);
                 var_dump($command);
                 die();
                 // echo $output;
